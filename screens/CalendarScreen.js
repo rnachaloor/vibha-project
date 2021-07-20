9bce2b6236cdefb1cf09312fba83ea113544eb26
varun <varun.vudathu@gmail.com>
2021-07-19 21:49:51 -0400

    calendar date select start

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -18,39 +18,53 @@ import BlackButton from '../components/BlackButton';
 import HomeHeader from '../components/HomeHeader';
 
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-
-const returnDateString = () => {
-}
-
-let day = returnDateString()
+import moment from 'moment';
 
 const CalendarScreen = ({navigation}) => {
+  this.state = {
+    selected_date: '2021-07-19',
+    markedDates: {'2021-07-19': {selected: true}},
+  };
+
+  const getSelectedDay = date => {
+    let markedDates = {};
+    markedDates[date] = {selected: true};
+    let serviceDate = moment(date);
+    serviceDate = serviceDate.format('YYYY.MM.DD');
+    this.setState({
+      selectedDate: serviceDate,
+      markedDates: markedDates,
+    });
+  };
+
   return (
     <SafeAreaView style={[styles.container, {flexDirection: 'column'}]}>
-      <HomeHeader onPress={() => navigation.openDrawer()}/>
+      <HomeHeader onPress={() => navigation.openDrawer()} />
       <View style={styles.otherbg}>
         <View style={styles.largeSpacing}></View>
         <Text style={styles.titleText}>Calendar</Text>
         <View style={styles.largeSpacing}></View>
-        <Calendar 
-            minDate={Date()} 
-            onPressArrowLeft={subtractMonth => subtractMonth()} 
-            onPressArrowRight={addMonth => addMonth()} 
-            enableSwipeMonths={true}
-            markedDates={{
-                "2021-07-19" : {selected: true, marked: true, selectedColor: '#D5B537'}
-            }}
-            style={styles.calendar}
-            theme={{
-                backgroundColor: '#8839BF',
-                calendarBackground: '#8839BF',
-                arrowColor: '#D5B537',
-                selectedDayBackgroundColor: '#D5B537',
-                selectedDayTextColor: 'black',
-                monthTextColor: '#D5B537',
-                textDisabledColor: 'gray',
-                dayTextColor: 'white',
-            }}/>
+        <Calendar
+          minDate={Date()}
+          onPressArrowLeft={subtractMonth => subtractMonth()}
+          onPressArrowRight={addMonth => addMonth()}
+          enableSwipeMonths={true}
+          markedDates={this.state.selected_date}
+          style={styles.calendar}
+          theme={{
+            backgroundColor: '#8839BF',
+            calendarBackground: '#8839BF',
+            arrowColor: '#D5B537',
+            selectedDayBackgroundColor: '#D5B537',
+            selectedDayTextColor: 'black',
+            monthTextColor: '#D5B537',
+            textDisabledColor: 'gray',
+            dayTextColor: 'white',
+          }}
+          onDayPress={day => {
+            getSelectedDay(day.dateString);
+          }}
+        />
       </View>
     </SafeAreaView>
   );
